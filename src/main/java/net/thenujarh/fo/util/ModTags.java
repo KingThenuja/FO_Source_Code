9e2875edfcbdbc04fbb4bc8e1275924ecaba9dd3
Thenuja <hewapathiranat@csdmedu.ca>
2025-05-24 14:28:36 -0400

    Commit changes

--- src/main/java/net/thenujarh/fo/util/ModTags.java
+++ src/main/java/net/thenujarh/fo/util/ModTags.java
@@ -9,20 +9,25 @@ import net.thenujarh.fo.FuturisticOres;
 
 public class ModTags {
     public static class Blocks {
+        public static final TagKey<Block> NEEDS_LEAD_TOOL = createTag("needs_lead_tool");
+        public static final TagKey<Block> INCORRECT_FOR_LEAD_TOOL = createTag("incorrect_for_lead_tool");
+        public static final TagKey<Block> NEEDS_RUBY_TOOL = createTag("needs_ruby_tool");
+        public static final TagKey<Block> INCORRECT_FOR_RUBY_TOOL = createTag("incorrect_for_ruby_tool");
+
 
         private static TagKey<Block> createTag(String name) {
             return TagKey.of(RegistryKeys.BLOCK, Identifier.of(FuturisticOres.MOD_ID, name));
         }
     }
 
+
+
+
     public static class Items {
 
         private static TagKey<Item> createTag(String name) {
             return TagKey.of(RegistryKeys.ITEM, Identifier.of(FuturisticOres.MOD_ID, name));
-        }
-
-    }
-
+        }}
     private static TagKey<Item> createTag(String name) {
         return TagKey.of(RegistryKeys.ITEM, Identifier.of(FuturisticOres.MOD_ID, name));
     }
