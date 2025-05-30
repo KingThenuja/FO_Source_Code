e36fc2790fb1e63cd3f759b97001586ad28c3604
Thenuja <hewapathiranat@csdmedu.ca>
2025-05-29 20:26:19 -0400

    Commit changes

--- src/main/java/net/thenujarh/fo/item/ModArmorMaterials.java
+++ src/main/java/net/thenujarh/fo/item/ModArmorMaterials.java
@@ -24,7 +24,16 @@ public class ModArmorMaterials {
                 map.put(ArmorItem.Type.HELMET, 2);
                 map.put(ArmorItem.Type.BODY, 4);
             }), 20, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, () -> Ingredient.ofItems(ModItems.LEAD),
-                    List.of(new ArmorMaterial.Layer(Identifier.of(FuturisticOres.MOD_ID, "pink_garnet"))), 0,0));
+                    List.of(new ArmorMaterial.Layer(Identifier.of(FuturisticOres.MOD_ID, "lead"))), 0,0));
+    public static final RegistryEntry<ArmorMaterial> RUBY_ARMOR_MATERIAL = registerArmorMaterial("ruby",
+            () -> new ArmorMaterial(Util.make(new EnumMap<>(ArmorItem.Type.class), map -> {
+                map.put(ArmorItem.Type.BOOTS, 2);
+                map.put(ArmorItem.Type.LEGGINGS, 4);
+                map.put(ArmorItem.Type.CHESTPLATE, 6);
+                map.put(ArmorItem.Type.HELMET, 2);
+                map.put(ArmorItem.Type.BODY, 4);
+            }), 20, SoundEvents.ITEM_ARMOR_EQUIP_NETHERITE, () -> Ingredient.ofItems(ModItems.LEAD),
+                    List.of(new ArmorMaterial.Layer(Identifier.of(FuturisticOres.MOD_ID, "ruby"))), 0,0));
 
 
     public static RegistryEntry<ArmorMaterial> registerArmorMaterial(String name, Supplier<ArmorMaterial> material) {
